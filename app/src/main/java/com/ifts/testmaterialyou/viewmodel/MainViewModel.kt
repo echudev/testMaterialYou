be56880bf611ddcb307611b68f0428eeda8d0d88
Ezequiel <ezequiel.maranda@gmail.com>
2024-10-05 14:59:28 -0300

    first commit

---
+++ app/src/main/java/com/ifts/testmaterialyou/viewmodel/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.livedatamaterialyoufragments.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _counter = MutableLiveData<Int>()
+    val counter: LiveData<Int> get() = _counter
+
+    init {
+        _counter.value = 0
+    }
+
+    fun incrementCounter() {
+        _counter.value = (_counter.value ?: 0) + 1
+    }
+}
+
